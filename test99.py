119fcd5be4d2b489c7536e9fb16527d0477e9bd4
jmcclenon <mars.john2018@gmail.com>
2022-04-20 16:10:33 -0400

    Dynamic route in about function

--- test99.py
+++ test99.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def hello():
     return '<h1>Numbers Pro v0.1.0</h1>'
 
-@app.route('/about')
-def about_page():
-    return '<h1>About Numbers Pro v0.1.0</h1>'
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>About Numbers Pro v0.1.0 for {username}</h1>'
     
 if __name__ == '__main__':
     app.run(debug=True)
