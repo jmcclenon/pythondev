3c86ab182395bd835ac6c854c2842ce1f4473abb
jmcclenon <mars.john2018@gmail.com>
2022-04-20 16:06:15 -0400

    About ROUTE added

--- test99.py
+++ test99.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'JohnnyO... Hello World! This is your world'
+    return '<h1>Numbers Pro v0.1.0</h1>'
+
+@app.route('/about')
+def about_page():
+    return '<h1>About Numbers Pro v0.1.0</h1>'
     
 if __name__ == '__main__':
     app.run(debug=True)
