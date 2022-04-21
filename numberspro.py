350de2a6549ae7eba52b0e29187a8605d6e3ef3a
jmcclenon <mars.john2018@gmail.com>
2022-04-21 08:36:58 -0400

    Added new page routes to numberspro.py

--- numberspro.py
+++ numberspro.py
@@ -1,14 +1,35 @@
+# NumbersPro.py
+# Development version 1.0
+# Flask route code to HTML files that make up this app
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Route to the Home page. Viewer can use either '/' or '/home'
 @app.route('/')
 @app.route('/home')
 def homepage():
     return render_template('home.html')
 
+# Route to the About page
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+# Route to the Sign-in page
+@app.route('/signin')
+def about_page():
+    return render_template('signin.html')
+
+# Route to the Purchase page
+@app.route('/purchase')
+def about_page():
+    return render_template('purchase.html')
+
+# Route to the Documentation page
+@app.route('/docs')
+def about_page():
+    return render_template('docs.html')
 
     
 if __name__ == '__main__':
